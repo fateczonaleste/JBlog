f543f3030bf5f672109b0d486cf0213e089048a9
eas-elias <eas-lias@hotmail.com>
2014-05-16 21:33:28 -0300

    conexao certa

--- src/br/jblog/dao/Conexao.java
+++ src/br/jblog/dao/Conexao.java
@@ -5,17 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-	
-	public static Connection getConnection(){
-		try{
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			return DriverManager.getConnection("url","banco","senha");
-		}catch(SQLException e){
-			e.printStackTrace();
-			return null;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			return null;
-		}	
+
+	private static Connection con;
+
+	public static Connection getConnection() {
+		if (con == null) {
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				con = (Connection) DriverManager.getConnection(
+						"jdbc:oracle:thin:@127.0.0.1:1521:xe", "system", "FATEC");
+
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return con;
 	}
+
 }
